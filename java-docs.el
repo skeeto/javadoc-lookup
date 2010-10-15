d7446762a1d1e474fb25cc8df1b4bbf0916b45d7
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-14 20:17:17 -0400

    Added big comment documentation header.

--- java-docs.el
+++ java-docs.el
@@ -1,8 +1,65 @@
-;(java-docs-dirs "~/src/chess/doc/javadoc" "/usr/share/doc/openjdk-6-jdk/api")
+;;; java-docs.el --- Java documentation Emacs integration
 
-;;; Code
+;; Copyright (C) 2010 Christopher Wellons <mosquitopsu@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; Permission to use, copy, modify, and distribute this software for
+;; any purpose with or without fee is hereby granted, provided that
+;; the above copyright notice and this permission notice appear in all
+;; copies.
+
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+;; WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+;; AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+;; CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+;; OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+;; NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+;; CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+;;; Commentary:
+
+;; This package provides a quick way to look up any Javadoc
+;; documentation from Emacs, using your browser to display the
+;; information.
+
+;; To install, just drop somewhere on your `load-path' and put this in
+;; your .emacs,
+
+;; (require 'java-docs)
+
+;; Give `java-docs' your root Java documentation directories. It will
+;; scan and index those directories, exposing them to
+;; `java-docs-lookup'. You can provide multiple directories at once,
+;; for example,
+
+;;   (java-docs "/usr/share/doc/openjdk-6-jdk/api" "~/src/project/doc")
+
+;; For your convenience, lookups can then be done with C-h j.
+
+;; When viewing docs, a browser is fired off with `browse-url'. You
+;; may have to set your `browse-url-browser-function' to make this
+;; work properly. For example,
+
+;;   (setq browse-url-browser-function 'browse-url-firefox)
+
+;; I strongly recommend using ido-mode in conjunction with
+;; java-docs. It becomes really easy to type in what you're looking
+;; for. To set this up properly, make sure ido-mode is enabled before
+;; loading java-docs.
+
+;; If you're using a recent enough version of emacs, the indexes will
+;; be cached in the directory specified in `java-docs-cache-dir', for
+;; the purpose of faster loading. If the documentation is updated,
+;; just delete the cache directory and it will be recreated.
+
+;;; Code:
 
 (provide 'java-docs)
+
+(require 'cl)
+(require 'ido)
 
 (global-set-key "\C-hj" 'java-docs-lookup)
 
@@ -15,10 +72,6 @@
 (defvar java-docs-cache-dir "~/.java-docs"
   "Location to store index information.")
 
-(defvar java-docs-dirs nil
-  "List of directories containing documentation. These
-directories are indexed, so do not edit this list directly.")
-
 (defvar java-docs-index (make-hash-table :test 'equal)
   "Index of documentation for quick lookups.")
 
@@ -29,7 +82,7 @@ directories are indexed, so do not edit this list directly.")
   (if ido-mode 'ido-completing-read 'completing-read)
   "Function used when performing a minibuffer read.")
 
-(defun java-docs-dirs (&rest dirs)
+(defun java-docs (&rest dirs)
   "Set the Javadoc search path to DIRS and index them."
   (dolist (dir dirs)
     (java-docs-add dir)))
