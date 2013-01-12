98d2f9a56a6c72d1f6f97b6a228e9ea5b86fa2f9
Christopher Wellons <mosquitopsu@gmail.com>
2013-01-12 17:00:42 -0500

    Fill out maven-fetch.el more.

--- maven-fetch.el
+++ maven-fetch.el
@@ -1,10 +1,34 @@
+;;; maven-fetch.el --- Fetch Javadoc artifacts from the Maven repository
+
+;; This is free and unencumbered software released into the public domain.
+
+;;; Commentary:
+
+;; This code adds support to javadoc-lookup by automatically fetching
+;; and indexing documentation artifacts from the Maven repository. It
+;; requires Maven and the command-line unzip utility installed on your
+;; system. If these are not in your $PATH, the variables
+;; `maven-program-name' and `unzip-program-name' will need to be set.
+
+;; An artifact is specified by a sequence (list or vector) of three
+;; strings: [groupId artifactId version]. For example,
+
+;;     ["org.apache.commons" "commons-math3" "3.0"]
+
+;; In your Emacs initialization file, call `javadoc-add-artifact' with
+;; your desired artifacts. They are only downloaded, unzipped, and
+;; indexed -- a slow process -- the very first time. Each startup
+;; after that, Emacs will work entirely from a cache (fast).
+
+;;; Code:
+
 (require 'cl)
 (require 'javadoc-lookup)
 
-(defvar maven-program-name "mvn"
+(defcustom maven-program-name "mvn"
   "Path to the Maven executable.")
 
-(defvar unzip-program-name "unzip"
+(defcustom unzip-program-name "unzip"
   "Path to the unzip executable.")
 
 (defvar maven-fetch-command
@@ -13,18 +37,17 @@
 
 (defun maven-fetch-artifact-jar (artifact)
   "Return the cache filename for ARTIFACT."
-  (expand-file-name (apply #'format "%s-%s-javadoc.jar" (cdr artifact))
-                    (jdl/dir-truename javadoc-lookup-cache-dir)))
+  (let ((file (format "%s-%s-javadoc.jar" (elt artifact 1) (elt artifact 2))))
+    (expand-file-name file (jdl/dir-truename javadoc-lookup-cache-dir))))
 
 (defun maven-fetch-unpacked-path (artifact)
   "Return the unpacked directory name for ARTIFACT."
-  (expand-file-name (apply #'format "%s/%s" (butlast artifact))
+  (expand-file-name (format "%s/%s" (elt artifact 0) (elt artifact 1))
                     (jdl/dir-truename javadoc-lookup-cache-dir)))
 
 (defun* maven-fetch (artifact)
   "Use maven to fetch ARTIFACT into the cache directory,
-returning true on success. An artifact is a list of three
-strings: (groupId artifactId version)."
+returning true on success."
   (let ((jarfile (maven-fetch-artifact-jar artifact)))
     (if (file-exists-p jarfile)
         (message "Artifact %S already downloaded" artifact)
@@ -34,11 +57,12 @@ strings: (groupId artifactId version)."
          (call-process maven-program-name nil nil nil
                        maven-fetch-command
                        (format "-Ddest=%s" jarfile)
-                       (apply #'format artifact-arg artifact)))))))
+                       (apply #'format artifact-arg
+                              (coerce artifact 'list))))))))
 
 (defun maven-fetch-unpack (artifact)
-  "Unpack an artifact in the javadoc-lookip cache directory,
-returning the destination directory. See `maven-fetch'."
+  "Unpack an artifact in the javadoc-lookup cache directory,
+returning the destination directory. Throws an error on any failure."
   (let* ((jarfile (maven-fetch-artifact-jar artifact))
          (destdir (maven-fetch-unpacked-path artifact)))
     (if (not (file-exists-p jarfile))
@@ -52,10 +76,16 @@ returning the destination directory. See `maven-fetch'."
           (delete-directory destdir t)
           (error "Failed to unpack %s" jarfile))))))
 
-(defun javadoc-lookup-maven-add (&rest artifacts)
+;;;###autoload
+(defun javadoc-add-artifact (&rest artifacts)
+  "Add Maven repository artifacts to the javadoc-lookup index.
+An artifact is specified by a sequence of three strings:
+ [groupId artifactId version]."
   (dolist (artifact artifacts)
     (if (maven-fetch artifact)
         (javadoc-add (maven-fetch-unpack artifact))
       (error "Failed to fetch %S" artifact))))
 
-;(javadoc-lookup-maven-add '("org.xerial" "sqlite-jdbc" "3.7.2"))
+(provide 'maven-fetch)
+
+;;; maven-fetch.el ends here
